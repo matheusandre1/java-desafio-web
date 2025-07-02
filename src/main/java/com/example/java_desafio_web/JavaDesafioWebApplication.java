1eb53632b63ead135500531917d14d8083a27323
Matheus Andre <matheusandr2@gmail.com>
2025-07-02 17:36:15 -0300

    feat: fist commit

---
+++ src/main/java/com/example/java_desafio_web/JavaDesafioWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_desafio_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesafioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesafioWebApplication.class, args);
+	}
+
+}
